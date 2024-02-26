f86322f19d3e77edbcd954d8d2951d16c35f3efe
Ashirvad <ashirvadsingh000@gmail.com>
2024-02-26 21:53:40 +0530

    Initial commit

---
+++ admin.js
@@ -0,0 +1,13 @@
+const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
+
+sideLinks.forEach(item => {
+    const li = item.parentElement;
+    item.addEventListener('click', () => {
+        sideLinks.forEach(i => {
+            i.parentElement.classList.remove('active');
+        })
+        li.classList.add('active');
+    })
+});
+
+
